0b7c0578a973a1f30bf97197eb5ea35b1f16fd26
Sepehr Laal <sepehr.laal@gmail.com>
2022-02-04 13:27:39 -0800

    added a simple working test, synthesizing a stack

--- index.test.js
+++ index.test.js
@@ -6,7 +6,27 @@ describe("cdk-web tests", () => {
     await page.goto(CDK_WEB_URL);
   });
 
-  it('sanity test', async () => {
+  it("sanity test", async () => {
     await expect(page.title()).resolves.toMatch("cdk-web");
+  });
+
+  it("should be able to synthesize a basic stack", async () => {
+    const factory = async () => {
+      const cdk = require("aws-cdk-lib");
+      const { aws_ec2: ec2, aws_sqs: sqs, aws_sns: sns, aws_s3: s3 } = cdk;
+      const app = new cdk.App(),
+        stack = new cdk.Stack(app, "BrowserStack"),
+        vpc = new ec2.Vpc(stack, "VPC"),
+        queue = new sqs.Queue(stack, "Queue"),
+        topic = new sns.Topic(stack, "Topic"),
+        bucket = new s3.Bucket(stack, "Bucket"),
+        assembly = app.synth();
+      return assembly.getStackArtifact(stack.stackName).template;
+    };
+    const [pageTemplate, nodeTemplate] = await Promise.all([
+      factory(),
+      page.evaluate(factory),
+    ]);
+    expect(pageTemplate).toMatchObject(nodeTemplate);
   });
 });
