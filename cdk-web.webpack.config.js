5716ed59c76684d4ddbc92c13da05e7d609b661e
Sepehr Laal <sepehr.laal@gmail.com>
2022-02-26 15:19:08 -0800

    started working on bundling a bundled .d.ts for the whole module

--- cdk-web.webpack.config.js
+++ cdk-web.webpack.config.js
@@ -6,28 +6,41 @@ const path = require("path");
 const shell = require("shelljs");
 const webpack = require("webpack");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const { Generator: TypingsGenerator } = require("npm-dts");
 
 const DEBUG = process.env.CDK_WEB_DEBUG !== undefined;
 DEBUG && console.log(">> building in DEBUG mode <<");
 
-const getModules = _.memoize((packageName = "") => {
+const getModules = _.memoize(() => {
   const { stdout: folders } = shell.exec(`find -type d -maxdepth 1`, {
-    cwd: path.resolve(__dirname, `node_modules/${packageName}`),
+    cwd: path.resolve(__dirname, `node_modules/${"aws-cdk-lib"}`),
     silent: true,
   });
   const paths = [
-    packageName,
+    "fs",
+    "path",
+    "constructs",
+    "aws-cdk-lib",
     ...folders
       .trim()
       .split("\n")
-      .map((p) => p.replace("./", `${packageName}/`))
+      .map((p) => p.replace("./", `${"aws-cdk-lib"}/`))
       .filter((m) => m !== ".")
       .filter((m) =>
         _.chain(() => require.resolve(m))
           .attempt()
           .isError()
       ),
-  ];
+  ].filter((m) => {
+    try {
+      require(m);
+      console.log(`[x] ${m}`);
+      return true;
+    } catch (err) {
+      console.log(`[ ] ${m}`);
+      return false;
+    }
+  });
   return paths;
 });
 
@@ -69,7 +82,7 @@ const getAssets = _.memoize(() => {
   return assets;
 });
 
-const entryPointTemplate = function (window = {}) {
+const entryPointLibrary = function (window = {}) {
   try {
     /* ASSETS */ //  <- json assets required at runtime by cdk to exist on disk memfs
     /* VERSION */ // <- version of libraries transpiled and exported from this module
@@ -87,21 +100,11 @@ const entryPointTemplate = function (window = {}) {
   }
 };
 
-const entryPointFunction = entryPointTemplate
+const entryPointFunction = entryPointLibrary
   .toString()
   .replace(
     "/* IMPORTS */",
-    `const imports = {\n${["fs", "path", "constructs", ...getModules("aws-cdk-lib")]
-      .filter((path) => {
-        try {
-          require(path);
-          console.log(`[x] ${path}`);
-          return true;
-        } catch (err) {
-          console.log(`[ ] ${path}`);
-          return false;
-        }
-      })
+    `const imports = {\n${getModules()
       .map((packageName) => `"${packageName}": require("${packageName}")`)
       .join(",")}};`
   )
@@ -128,8 +131,25 @@ const entryPointFunction = entryPointTemplate
 
 const entryPointPath = path.resolve(__dirname, "index.generated.js");
 const entryPointText = `;require("idempotent-babel-polyfill");(${entryPointFunction.toString()})(window);`;
-
 fs.writeFileSync(entryPointPath, entryPointText, { encoding: "utf-8" });
+
+fs.writeFileSync(
+  path.resolve(__dirname, "index.generated.cjs"),
+  `module.exports=(${function () {
+    return {
+      "aws-cdk": require("./cdk-web-cli"),
+      /* EXPORTS */
+    };
+  }
+    .toString()
+    .replace(
+      "/* EXPORTS */",
+      getModules()
+        .map((packageName) => `"${packageName}": require("${packageName}")`)
+        .join(",\n")
+    )})();`,
+  { encoding: "utf-8" }
+);
 
 module.exports = {
   node: {
@@ -192,10 +212,32 @@ module.exports = {
       "process.version": `"${process.version}"`,
     }),
     {
-      apply: (compiler) => {
+      apply: function (compiler) {
         compiler.hooks.afterEmit.tap("AfterEmitPlugin", () => {
-          console.log("copying the bundle out for playground React app");
-          shell.cp(path.resolve(__dirname, "dist/cdk-web.js"), path.resolve(__dirname, "public"));
+          setTimeout(() => {
+            // post build scripts go here
+            console.log("copying the bundle out for playground React app");
+            shell.cp(path.resolve(__dirname, "dist/cdk-web.js"), path.resolve(__dirname, "public"));
+            console.log("generation typings");
+            new TypingsGenerator(
+              {
+                entry: path.resolve(__dirname, "index.generated.cjs"),
+                logLevel: "debug",
+              },
+              true /* enable logs */,
+              true /* throw error */
+            )
+              .generate()
+              .then(() => {
+                console.log("removing incorrect typings");
+                const typings = path.resolve(__dirname, "index.d.ts");
+                fs.writeFileSync(
+                  typings,
+                  fs.readFileSync(typings, { encoding: "utf-8" }).replace(/declare.*\.d\..*$\n.*\n}/gm, ""),
+                  { encoding: "utf-8" }
+                );
+              });
+          });
         });
       },
     },
