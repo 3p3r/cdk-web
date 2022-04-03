140c0d40156e8956db5e92784562ea990f48771a
Sepehr Laal <sepehr.laal@gmail.com>
2022-04-02 19:35:13 -0700

    more documentation + check if we have any composites before turning the method async

--- webpack/modules/stage.js
+++ webpack/modules/stage.js
@@ -1,16 +1,24 @@
 const stage = require("../../node_modules/aws-cdk-lib/core/lib/stage");
 
+/** WebStage overrides Stage to make its synth() method asynchronous */
 class WebStage extends stage.Stage {
-  async synth(...args) {
-    const assembly = super.synth(...args);
-    await Promise.all(
-      this.node
-        .findAll()
-        .map((construct) =>
-          construct.constructor.Compose ? construct.constructor.Compose(construct) : Promise.resolve()
-        )
-    );
-    return assembly;
+  /**
+   * async synth() override over cdk's sync synth()
+   * @param {stage.StageSynthesisOptions} options
+   * @param  {...any} args
+   * @returns CloudAssembly (sync version) or a Promise<CloudAssembly> in case tree contains async constructs
+   */
+  synth(options, ...args) {
+    const assembly = super.synth(options, ...args);
+    const allNodes = this.node.findAll();
+    const compositeNodes = allNodes.filter((construct) => "function" === typeof construct.constructor.Compose);
+    if (compositeNodes.length > 0) {
+      return Promise.all(compositeNodes.map((construct) => construct.constructor.Compose(construct))).then(
+        () => assembly
+      );
+    } else {
+      return assembly;
+    }
   }
 }
 
