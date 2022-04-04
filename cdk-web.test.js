b632f4d0e09d7dbe80b7475f5348b2f30b7a9699
Sepehr Laal <sepehr.laal@gmail.com>
2022-04-04 13:40:50 -0700

    added test for path changes moving forward

--- cdk-web.test.js
+++ cdk-web.test.js
@@ -219,4 +219,40 @@ describe("cdk-web tests", () => {
     await expect(page.evaluate(factory, 0)).resolves.toBeUndefined();
     await expect(page.evaluate(factory, 1)).rejects.toThrow(/evaluation failed/gi);
   });
+
+  it("should only touch '/cdk', '/tmp', and '/cdk.out' in memory", async () => {
+    async function factory() {
+      const cdk = CDK.require("aws-cdk-lib");
+      const ec2 = CDK.require("aws-cdk-lib/aws-ec2");
+      const sqs = CDK.require("aws-cdk-lib/aws-sqs");
+      const sns = CDK.require("aws-cdk-lib/aws-sns");
+      const s3 = CDK.require("aws-cdk-lib/aws-s3");
+      const app = new cdk.App();
+      const stack = new cdk.Stack(app, "BrowserStack");
+      new ec2.Vpc(stack, "VPC");
+      new sqs.Queue(stack, "Queue");
+      const cli = new CDK.PseudoCli({ stack });
+      await cli.synth();
+    }
+
+    await expect(
+      page.evaluate(async () => {
+        CDK.free();
+        const fs = CDK.require("fs"); // calls init() internally
+        const dirs = ["/cdk.out", "/tmp"];
+        for (const dir of dirs) if (fs.readdirSync(dir).length !== 0) throw Error(`${dir} is not empty`);
+      })
+    ).resolves.toBeUndefined();
+    await expect(page.evaluate(factory)).resolves.toBeUndefined();
+    await expect(
+      page.evaluate(async () => {
+        const vol = CDK.require("fs").vol.toJSON();
+        const dirs = ["/cdk", "/cdk.out", "/tmp"];
+        const files = Object.keys(vol);
+        if (files.length < dirs.length && files.some((file) => !dirs.some((dir) => file.startsWith(dir))))
+          throw Error(JSON.stringify(files));
+        CDK.free();
+      })
+    ).resolves.toBeUndefined();
+  });
 });
