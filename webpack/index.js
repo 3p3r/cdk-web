5d78bd1c518eb34cab0e72e3cf4f6cdc76d88adc
Sepehr Laal <sepehr.laal@gmail.com>
2022-04-04 01:30:14 -0700

    removing modules as they're going to change a lot

--- webpack/index.js
+++ webpack/index.js
@@ -1,11 +1,6 @@
 module.exports = {
-  generateEntrypoint: require("./generate-entrypoint"),
   common: require("./common"),
-  modules: {
-    os: require("./modules/os"),
-    fs: require("./modules/fs"),
-    empty: require("./modules/empty"),
-  },
+  generateEntrypoint: require("./generate-entrypoint"),
   loaders: {
     empty: require("./loaders/empty-loader"),
     override: require("./loaders/override-loader"),
