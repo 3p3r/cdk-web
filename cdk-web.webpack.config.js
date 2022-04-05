9bcd1cc0d60aa66f67176146b051e55753f1b06e
Sepehr Laal <sepehr.laal@gmail.com>
2022-04-05 16:36:11 -0700

    fixed the infinite compile loop issue in "npm run dev:framework"

--- cdk-web.webpack.config.js
+++ cdk-web.webpack.config.js
@@ -71,6 +71,7 @@ module.exports = {
     },
   },
   plugins: [
+    ...(common.__SERVER ? [new plugins.WebpackMildCompile()] : []),
     new plugins.PostBuildPlugin(),
     new plugins.ExtendedAliasPlugin(),
     new webpack.ProgressPlugin(),
@@ -101,7 +102,8 @@ module.exports = {
         test: loaders.override.KeepTrack(__("node_modules/@mhlabs/cfn-diagram/graph/Vis.js")),
         options: {
           search: /if\s+\(standaloneIndex\)([^]*)else/gm,
-          replace: "if(standaloneIndex){fs.writeFileSync(path.join(uiPath,'index.html'),fs.readFileSync('/ui/render-template.html','utf8').replace(/%DATA_JS%/g, fileContent),'utf8')}else",
+          replace:
+            "if(standaloneIndex){fs.writeFileSync(path.join(uiPath,'index.html'),fs.readFileSync('/ui/render-template.html','utf8').replace(/%DATA_JS%/g, fileContent),'utf8')}else",
         },
       },
       {
