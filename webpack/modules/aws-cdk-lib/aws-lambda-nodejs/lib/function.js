45b478c200a650821da22836c779150ddec2a67b
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2022-04-09 21:55:08 -0700

    in browser, allow the fetch path URL to be overriden. node not implemented yet

--- webpack/modules/aws-cdk-lib/aws-lambda-nodejs/lib/function.js
+++ webpack/modules/aws-cdk-lib/aws-lambda-nodejs/lib/function.js
@@ -2,8 +2,15 @@ const original = require("../../../../../node_modules/aws-cdk-lib/aws-lambda-nod
 
 class WebNodejsFunction extends original.NodejsFunction {
   static async Compose(self) {
+    let fetchFunction;
     const { bundling } = self.code;
-    await bundling.init();
+    if ("undefined" !== typeof window) {
+      const wasmPath = window.CDK_WEB_ESBUILD_WASM || "esbuild.wasm";
+      fetchFunction = () => fetch(wasmPath);
+    } else {
+      throw new Error("not implemented");
+    }
+    await bundling.init(fetchFunction);
     const asset = self.node.children.filter((node) => node.assetPath).shift();
     if (!asset) {
       throw new Error(`Unable to find the asset of ${this.node.path}`);
