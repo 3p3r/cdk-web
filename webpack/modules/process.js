daa09ad421aa92bbffc5b7f85d5512b4e4c6f9ff
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2022-04-20 01:22:18 -0700

    seems like this hack eval is no longer needed. tests are passing

--- webpack/modules/process.js
+++ webpack/modules/process.js
@@ -3,9 +3,6 @@ const path = require("path");
 const konsole = require("./console-browserify/index");
 const proc = require("../../node_modules/process/browser");
 
-const isBrowser = typeof window !== "undefined";
-const nodeProcess = !isBrowser && eval("process");
-
 module.exports = {
   ...proc,
   chdir(dir) {
@@ -17,7 +14,6 @@ module.exports = {
     return _.ternary(this.listeners, this.listeners(sym).length, 0);
   },
   hrtime: require("browser-process-hrtime"),
-  env: _.ternary(isBrowser, proc.env, { ...nodeProcess.env, ...proc.env }),
   stderr: { write: konsole.log },
   stdout: { write: konsole.log },
 };
