e42460322dd0f17c72ac87aafb6315215bd306da
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2022-04-19 23:59:53 -0700

    adding coverage so tests pass

--- webpack/modules/globals.js
+++ webpack/modules/globals.js
@@ -1,3 +1,4 @@
+const utils = require("./utils");
 const createEvalExpression = (symbol) => `(typeof ${symbol}!=='undefined')&&${symbol}`;
 
 const contextProcess = eval(createEvalExpression("process"));
@@ -6,9 +7,9 @@ const contextRequire = eval(createEvalExpression("require"));
 const hasProcess = false !== (contextProcess && contextProcess.version && contextProcess.versions.node);
 const hasBrowser = typeof window !== undefined;
 
-const actualRequire = contextRequire || __webpack_require__;
-const actualProcess = hasProcess ? contextProcess : require("./process");
-const actualFs = hasProcess ? contextRequire("fs") : require("./fs");
+const actualRequire = utils.get(contextRequire, __webpack_require__);
+const actualProcess = utils.ternary(hasProcess, contextProcess, require("./process"));
+const actualFs = utils.ternary(hasProcess, contextRequire("fs"), require("./fs"));
 
 module.exports = {
   hasProcess,
